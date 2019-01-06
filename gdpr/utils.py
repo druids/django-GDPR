9804c3b680f0738076f94222967d497ce7e72634
Filip Dobrovolny <brnopcman@gmail.com>
2019-01-06 03:50:48 +0100

    Add typing to gdpr.utils and fix __import__ call

--- gdpr/utils.py
+++ gdpr/utils.py
@@ -1,7 +1,10 @@
-def str_to_class(class_string):
+from typing import Any
+
+
+def str_to_class(class_string: str) -> Any:
     module_name, class_name = class_string.rsplit('.', 1)
     # load the module, will raise ImportError if module cannot be loaded
-    m = __import__(module_name, globals(), locals(), str(class_name))
+    m = __import__(module_name, globals(), locals(), [str(class_name)])
     # get the class, will raise AttributeError if class cannot be found
     c = getattr(m, class_name)
     return c
