5de96d1e22be77bfcb1c3e2050a596dc6383367b
Filip Dobrovolny <brnopcman@gmail.com>
2019-02-18 14:55:18 +0100

    Improve runtests.py

--- runtests.py
+++ runtests.py
@@ -10,7 +10,5 @@ from django.test.utils import get_runner
 if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
     django.setup()
-    TestRunner = get_runner(settings)
-    test_runner = TestRunner()
-    failures = test_runner.run_tests(["tests", "gdpr"])
+    failures = TestRunner = get_runner(settings)().run_tests(["tests", "gdpr"])
     sys.exit(bool(failures))
