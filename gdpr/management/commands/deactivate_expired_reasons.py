a146736f120ca0c61c5e15beb88eaceabd333070
Zuzana Kašáková <zuza.kasakova@gmail.com>
2019-03-12 15:31:22 +0100

    Replace _expirement by expire method in command

--- gdpr/management/commands/deactivate_expired_reasons.py
+++ gdpr/management/commands/deactivate_expired_reasons.py
@@ -8,4 +8,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         for legal_reason in LegalReason.objects.filter_expired_retaining_data_in_last_days():
-            legal_reason._expirement()
+            legal_reason.expire()
